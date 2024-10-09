4b195b134c064ac033f99455a829904ee9198184
dahechma <mohamedamine.dahech@supcom.tn>
2024-10-09 10:14:48 +0200

    user

--- user/user.py
+++ user/user.py
@@ -4,13 +4,6 @@ import requests
 import json
 from werkzeug.exceptions import NotFound
 
-# CALLING gRPC requests
-import grpc
-from concurrent import futures
-import booking_pb2
-import booking_pb2_grpc
-import movie_pb2
-import movie_pb2_grpc
 
 # CALLING GraphQL requests
 # todo to complete
@@ -22,7 +15,26 @@ HOST = '0.0.0.0'
 
 with open('{}/data/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
+   
+
+@app.route("/movies", methods=['GET'])
+def get_movies():
+     
+      query = '''
+    query {
+        allMovies {
+            id
+            title
+            director
+            rating
+        }
+    }
+    '''
+      response  = requests.post("http://127.0.0.1:3200/graphql",json={'query':query })
+      return response.json()   
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
+   
+   
